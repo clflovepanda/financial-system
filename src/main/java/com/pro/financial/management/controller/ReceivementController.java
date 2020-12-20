e0a25a3976e4009cdf80201f718ce3d457b25f0a
suo <1466200463@qq.com>
2020-12-20 20:06:02 +0800

    到款管理

--- src/main/java/com/pro/financial/management/controller/ReceivementController.java
+++ src/main/java/com/pro/financial/management/controller/ReceivementController.java
@@ -2,6 +2,7 @@ package com.pro.financial.management.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.pro.financial.management.biz.*;
+import com.pro.financial.management.controller.view.ReceivementView;
 import com.pro.financial.management.dao.entity.ReceivementEntity;
 import com.pro.financial.management.dao.entity.ReceivementTypeEntity;
 import com.pro.financial.management.dao.entity.RemitterMethodEntity;
@@ -9,15 +10,16 @@ import com.pro.financial.management.dao.entity.SubscriptionLogEntity;
 import com.pro.financial.management.dto.AccountingLogDto;
 import com.pro.financial.management.dto.ReceivementDto;
 import com.pro.financial.user.dao.entity.CompanyEntity;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.crypto.Data;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -53,6 +55,8 @@ public class ReceivementController {
             @RequestParam("flag") Integer flag) {
         JSONObject result = new JSONObject();
         ReceivementDto receivementDto = JSONObject.parseObject(jsonInfo.toJSONString(), ReceivementDto.class);
+        receivementDto.setCreateUser(userId);
+        receivementDto.setCtime(new Date());
         receivementDto.setUpdateUser(userId);
         receivementDto.setUtime(new Date());
         if (flag == 1) {
@@ -72,21 +76,69 @@ public class ReceivementController {
     public JSONObject getList(HttpServletRequest request) {
         JSONObject result = new JSONObject();
         // TODO 根据用户权限获取到能看到的到款id列表
-        List<Integer> ids = new ArrayList<>();
-        List<ReceivementEntity> receivementEntities = receivementBiz.getListById(ids);
+        // List<Integer> ids = new ArrayList<>();
+        List<ReceivementView> receivementViewList = new ArrayList<>();
+        List<ReceivementEntity> receivementEntities = receivementBiz.getAllList();
+        if (CollectionUtils.isEmpty(receivementEntities)) {
+            result.put("code", HttpStatus.OK.value());
+            result.put("msg", HttpStatus.OK.getReasonPhrase());
+            result.put("data", receivementEntities);
+            return result;
+        }
         List<Integer> receivementIds = receivementEntities.stream().map(ReceivementEntity::getId).collect(Collectors.toList());
-        // TODO 公司列表查询
-        List<CompanyEntity> companyEntities = new ArrayList<>();
-        List<ReceivementTypeEntity> receivementTypeEntities = receivementTypeBiz.getList();
-        List<RemitterMethodEntity> remitterMethodEntities = remitterMethodBiz.getList();
         List<SubscriptionLogEntity> subscriptionLogEntities = subscriptionLogBiz.getListByReceivementIds(receivementIds);
+        Map<Integer, List<SubscriptionLogEntity>> idAndSubscriptionLogEntitysMap = new HashMap<>();
+        if (!CollectionUtils.isEmpty(subscriptionLogEntities)) {
+            for (SubscriptionLogEntity entity : subscriptionLogEntities) {
+                int seid = entity.getReceivementId();
+                if (idAndSubscriptionLogEntitysMap.containsKey(seid)) {
+                    List<SubscriptionLogEntity> entities = idAndSubscriptionLogEntitysMap.get(seid);
+                    entities.add(entity);
+                } else {
+                    List<SubscriptionLogEntity> entities = new ArrayList<>();
+                    entities.add(entity);
+                    idAndSubscriptionLogEntitysMap.put(seid, entities);
+                }
+            }
+        }
+        for (ReceivementEntity entity : receivementEntities) {
+            ReceivementView view = new ReceivementView();
+            view.setId(entity.getId());
+            view.setCompanyId(entity.getCompanyId());
+            view.setReceivementTypeId(entity.getReceivementTypeId());
+            view.setReceivementMoney(entity.getReceivementMoney());
+            view.setRemitterMethodId(entity.getRemitterMethodId());
+            view.setRemitter(entity.getRemitter());
+            view.setReceiveDate(entity.getReceiveDate());
+            if (!CollectionUtils.isEmpty(idAndSubscriptionLogEntitysMap) && !CollectionUtils.isEmpty(idAndSubscriptionLogEntitysMap.get(entity.getId()))) {
+                BigDecimal hadSubscriptionTotalMoney = BigDecimal.ZERO;
+                Date latestSubscriptionTime = new Date("1970-01-01 00:00:00");
+                List<SubscriptionLogEntity> sentities = idAndSubscriptionLogEntitysMap.get(entity.getId());
+                for (SubscriptionLogEntity sentity : sentities) {
+                    hadSubscriptionTotalMoney.add(sentity.getReceivementMoney());
+                    if (latestSubscriptionTime.compareTo(sentity.getCtime()) < 0) {
+                        latestSubscriptionTime = sentity.getCtime();
+                    }
+                }
+                view.setHadSubscriptionTotalMoney(hadSubscriptionTotalMoney);
+                view.setLatestSubscriptionTime(latestSubscriptionTime);
+            } else {
+                view.setHadSubscriptionTotalMoney(BigDecimal.ZERO);
+                view.setLatestSubscriptionTime(new Date());
+            }
+            view.setRemaindSubscriptionTotalMoney(view.getReceivementMoney().subtract(view.getHadSubscriptionTotalMoney()));
+            view.setState(entity.getState());
+            view.setRemark(entity.getRemark());
+            view.setCtime(entity.getCtime());
+            view.setCreateUser(entity.getCreateUser());
+            view.setUpdateUser(entity.getUpdateUser());
+            view.setUtime(entity.getUtime());
+            receivementViewList.add(view);
+        }
+
         result.put("code", HttpStatus.OK.value());
         result.put("msg", HttpStatus.OK.getReasonPhrase());
-        result.put("receivementEntities", receivementEntities);
-        result.put("companyEntities", companyEntities);
-        result.put("receivementTypeEntities", receivementTypeEntities);
-        result.put("remitterMethodEntities", remitterMethodEntities);
-        result.put("subscriptionLogEntities", subscriptionLogEntities);
+        result.put("data", receivementEntities);
         return result;
     }
 
@@ -168,5 +220,15 @@ public class ReceivementController {
         result.put("code", HttpStatus.INTERNAL_SERVER_ERROR.value());
         result.put("msg", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
         return result;
+    }
+
+    public static void main(String[] args) {
+        Date date1 = new Date("1970/01/01 00:00:00");
+        Date date2 = new Date();
+        if (date1.compareTo(date2) < 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(2);
+        }
     }
 }
