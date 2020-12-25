5eda3817fea87f097eba2b15d4f1d190ffce7c95
yupeng <411500916@qq.com>
2020-12-25 23:27:52 +0800

    bug修改

--- src/main/java/com/pro/financial/management/controller/ReceivementController.java
+++ src/main/java/com/pro/financial/management/controller/ReceivementController.java
@@ -269,6 +269,8 @@ public class ReceivementController {
             return result;
         }
         subscriptionLogDto.setState(1);
+        subscriptionLogDto.setCreateUser(userId);
+        subscriptionLogDto.setCtime(new Date());
         int count = subscriptionLogBiz.addSubscriptionLog(subscriptionLogDto);
         // TODO 这里需要处理 如果是收入类型为预收押金，则自动进入到押金列表。收入类型为收回押金，则自动进入到支出管理-押金支出列表。
         // TODO 或者在收入支出押金列表中区别类型处理
