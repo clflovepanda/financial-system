56b8d8a7f376b40bc84144dce5a823dde0d867b7
yupeng <411500916@qq.com>
2021-01-04 20:57:09 +0800

    修改菜单层级

--- src/main/java/com/pro/financial/user/controller/PermissionController.java
+++ src/main/java/com/pro/financial/user/controller/PermissionController.java
@@ -4,10 +4,12 @@ import com.alibaba.fastjson.JSONObject;
 import com.pro.financial.user.biz.PermissionBiz;
 import com.pro.financial.user.dto.PermissionDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,12 +35,26 @@ public class PermissionController {
         JSONObject result = new JSONObject();
         List<PermissionDto> permissionDtos = permissionBiz.getFirstPermissionList();
         List<PermissionDto> permissionDtoAll = permissionBiz.getOtherPermissionList();
+        for (PermissionDto permissionDto : permissionDtoAll) {
+            for (PermissionDto permissionDto2 : permissionDtoAll) {
+                if (permissionDto.getPermissionId() - permissionDto2.getParentId() == 0) {
+                    if (CollectionUtils.isEmpty(permissionDto.getPermissionSons())) {
+                        List<PermissionDto> permissionsons = new ArrayList<>();
+                        permissionsons.add(permissionDto2);
+                        permissionDto.setPermissionSons(permissionsons);
+                    } else {
+                        permissionDto.getPermissionSons().add(permissionDto2);
+                    }
+                }
+            }
+        }
         for (PermissionDto permissionDto : permissionDtos) {
             List<PermissionDto> permissionDtoSons = new ArrayList<>();
             for (PermissionDto permissionDtoSon : permissionDtoAll) {
                 if (permissionDtoSon.getParentId() - permissionDto.getPermissionId() == 0) {
                     permissionDtoSons.add(permissionDtoSon);
                 }
+
             }
             permissionDto.setPermissionSons(permissionDtoSons);
         }
