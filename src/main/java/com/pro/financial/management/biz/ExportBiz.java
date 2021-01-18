68e7b253cb0760a2e0c73066cf6201c312ab4c9f
yupeng <411500916@qq.com>
2021-01-18 17:56:10 +0800

    设置表头

--- src/main/java/com/pro/financial/management/biz/ExportBiz.java
+++ src/main/java/com/pro/financial/management/biz/ExportBiz.java
@@ -10,10 +10,16 @@ import com.pro.financial.management.dto.ProjectDto;
 import com.pro.financial.management.dto.RevenueDto;
 import com.pro.financial.utils.ExportUtil;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.List;
 
@@ -210,6 +216,59 @@ public class ExportBiz {
         }
         result.put("code", 0);
         result.put("msg", "");
+        return result;
+    }
+
+    public static JSONObject exportStatisticsProject() {
+        JSONObject result = new JSONObject();
+        //设置到款统计的表头
+        HSSFWorkbook wb = new HSSFWorkbook();
+        HSSFSheet sheet = wb.createSheet();
+        HSSFRow row1 = sheet.createRow(0);
+        row1.createCell(0).setCellValue("id");
+        row1.createCell(1).setCellValue("到款");
+        row1.createCell(9).setCellValue("认款");
+        HSSFRow row2 = sheet.createRow(1);
+        row2.createCell(1).setCellValue("账户");
+        row2.createCell(2).setCellValue("汇款方");
+        row2.createCell(3).setCellValue("种类");
+        row2.createCell(4).setCellValue("人民币");
+        row2.createCell(5).setCellValue("外币");
+        row2.createCell(6).setCellValue("时间");
+        row2.createCell(7).setCellValue("备注");
+        row2.createCell(8).setCellValue("凭证号");
+        row2.createCell(9).setCellValue("名称");
+        row2.createCell(10).setCellValue("编号");
+        row2.createCell(11).setCellValue("收入类型");
+        row2.createCell(12).setCellValue("收入属性");
+        row2.createCell(13).setCellValue("收入部门");
+        row2.createCell(14).setCellValue("收入");
+        row2.createCell(15).setCellValue("押金");
+        row2.createCell(16).setCellValue("认款人");
+        row2.createCell(17).setCellValue("发票");
+        row2.createCell(18).setCellValue("时间");
+
+        HSSFCellStyle style = wb.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+        // 合并单元格：参数：起始行, 终止行, 起始列, 终止列
+        CellRangeAddress cra = new CellRangeAddress(0, 1, 0, 0);
+        sheet.addMergedRegion(cra);
+        CellRangeAddress cra1 = new CellRangeAddress(0, 0, 1, 8);
+        sheet.addMergedRegion(cra1);
+        CellRangeAddress cra2 = new CellRangeAddress(0, 0, 9, 18);
+        sheet.addMergedRegion(cra2);
+        row1.getCell(0).setCellStyle(style);
+        row1.getCell(1).setCellStyle(style);
+        row1.getCell(9).setCellStyle(style);
+        try {
+            FileOutputStream output=new FileOutputStream("expenditure.xls");
+            wb.write(output);
+            output.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return result;
     }
 }
